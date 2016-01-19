2dae0d08228fd4ad7cd7b8159c4735f4abbdf5b7
Geert-Jan Besjes <geert-jan.besjes@cern.ch>
2016-01-19 19:04:33 +0100

    add eventInfo struct

--- ZLFVAnalysis/ZLFVAnalysis/Utils.h
+++ ZLFVAnalysis/ZLFVAnalysis/Utils.h
@@ -8,6 +8,7 @@
 #include "xAODTracking/VertexFwd.h"
 #include "xAODJet/JetContainerInfo.h"
 
+#include "xAODEventInfo/EventInfo.h"
 #include "xAODRootAccess/TEvent.h"
 #include "xAODTracking/Vertex.h"
 
@@ -19,10 +20,20 @@ enum class RunPeriod { p7tev ,p8tev ,p13tev, INVALID };
 enum class DerivationTag { NotADerivation, p1872, p2353, p2363, p2372, p2375, p2377, p2384, p2419, p2425, INVALID_Derivation };
 
 namespace Utils {
+    typedef struct {
+        uint32_t RunNumber;
+        unsigned long long EventNumber;
+        uint32_t LumiBlockNumber;
+        uint32_t mc_channel_number;
+    } EventInfo;
+
     RunPeriod periodFromString(const std::string& period);
     DerivationTag derivationTagFromString(const std::string& tag);
     xAOD::JetInput::Type JetTypeFromString(const std::string& algname);
+
+    const xAOD::EventInfo* GetEventInfo(xAOD::TEvent& event);
     const xAOD::Vertex* GetPrimaryVertex(xAOD::TEvent& event);
+    const EventInfo ExtractEventInfo(const xAOD::EventInfo* info, bool isData = true);
 }
 
 #endif
